be8ccda38cb919f8042628825cbc7800efdafb6b
denya133 <denya@saifas.com>
2020-11-02 10:38:03 +0300

    Changed code for this specific addon

---
+++ src/interfaces/EmbeddableStaticInterface.js
@@ -0,0 +1,25 @@
+// This file is part of leanes-embeddable-addon.
+//
+// leanes-embeddable-addon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// leanes-embeddable-addon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with leanes-embeddable-addon.  If not, see <https://www.gnu.org/licenses/>.
+
+import type { CollectionInterface, RecordInterface } from '@leansdk/leanes-mapper-addon/src';
+import type { EmbedConfigT } from '../types/EmbedConfigT';
+
+export interface EmbeddableStaticInterface {
+  embeddings: {[key: string]: EmbedConfigT};
+
+  makeSnapshotWithEmbeds(aoRecord: ?RecordInterface): ?object
+
+  (aoAttributes: object, aoCollection: CollectionInterface): RecordInterface;
+}
---
+++ src/types/EmbedConfigT.js
@@ -0,0 +1,34 @@
+// This file is part of leanes-embeddable-addon.
+//
+// leanes-embeddable-addon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// leanes-embeddable-addon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with leanes-embeddable-addon.  If not, see <https://www.gnu.org/licenses/>.
+
+import type { JoiT } from '@leansdk/leanes/src';
+
+export type EmbedConfigT = {|
+  refKey: string,
+  attr: ?string,
+  inverse: string,
+  inverseType: ?string,
+  embedding: ('relatedEmbed' | 'relatedEmbeds' | 'hasEmbed' | 'hasEmbeds'),
+  through: ?[string, {|by: string|}],
+  putOnly: boolean,
+  loadOnly: boolean,
+  recordName: (recordType: ?string) => string,
+  collectionName: (recordType: ?string) => string,
+  validate: () => JoiT,
+  load: () => Promise<?any>,
+  put: () => Promise<void>,
+  restore: () => Promise<?any>,
+  replicate: () => object | object[]
+|}
